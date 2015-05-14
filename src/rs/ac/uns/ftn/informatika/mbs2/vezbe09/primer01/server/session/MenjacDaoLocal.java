b5461fc8acc8fd2f84ea682d66e95f1371893db7
Zieghailo <isakov.m@gmail.com>
2015-05-14 16:32:59 +0200

    Deleted old project files

--- src/rs/ac/uns/ftn/informatika/mbs2/vezbe09/primer01/server/session/MenjacDaoLocal.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.uns.ftn.informatika.mbs2.vezbe09.primer01.server.session;
-
-import rs.ac.uns.ftn.informatika.mbs2.vezbe09.primer01.server.entity.Menjac;
-
-public interface MenjacDaoLocal extends GenericDaoLocal<Menjac, Integer> {
-
-}
